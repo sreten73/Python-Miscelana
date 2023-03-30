81e4a445bceabb569533a78bc8254b2b878302cc
Sreten Jokic <sreten98@gmail.com>
2023-03-30 15:03:03 +0200

    correct name of the screenshot to valid date

--- 07.screenshot.py
+++ 07.screenshot.py
@@ -2,12 +2,15 @@
 import time
 import pyautogui
 import os
+import datetime
 
 
 def screenshot():
     script_directory = os.path.dirname(os.path.abspath(__file__))
-    name = int(round(time.time()*1000))
-    name = f'{script_directory}/{name}.png'
+    #    name = int(round(time.time()*1000))
+    name = round(time.time())
+    valid_date = str(datetime.datetime.fromtimestamp(name).date())
+    name = f'{script_directory}/{valid_date}.png'
     time.sleep(5)
     img = pyautogui.screenshot(name)
     img.show()
